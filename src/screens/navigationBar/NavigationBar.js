d1f995218a261858b35d9a441821b45a5585bb8e
ElmoMax <williamzhicay32@gmail.com>
2023-02-07 18:49:55 -0500

    Added navbar

---
+++ src/screens/navigationBar/NavigationBar.js
@@ -0,0 +1,32 @@
+ import React from 'react'
+ import './navigation.css'
+ // Bootstrap styling
+ import Navbar from 'react-bootstrap/Navbar'
+ import Nav from 'react-bootstrap/Nav'
+ // end Bootstrap styling
+ import { navigationLinks} from '../../helpers/navigationLinks'
+
+ function createLinks(){
+  return navigationLinks.map((e, idx) => ( // iterate trhough the element e
+    <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
+  ))
+ }
+ 
+ function NavigationBar() {
+   return (
+     <div id='home'>
+        <Navbar className='navigation_container' expand='md'>
+        <Navbar.Brand style={{marginLeft: '1rem'}} href="#home">Elmo Max</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse style={{justifyContent: 'flex-end', marginRight: '1rem'}}>
+          <Nav className='links' style={{margin: '0 1rem'}}>
+            {createLinks()}
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+     </div>
+   )
+ }
+ 
+ export default NavigationBar
+ 
