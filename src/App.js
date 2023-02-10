8b5662362bff6aef224a80a9782b2f4519fa8abc
ElmoMax <williamzhicay32@gmail.com>
2023-02-10 14:01:00 -0500

    Added techs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './screens/navigationBar/NavigationBar';
 import Particles from './Particles';
 import Header from './screens/header/Header';
 import AboutMe from './screens/aboutMe/aboutMe';
+import Technologies from './screens/technologies/Technologies';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Particles />
       <AboutMe />
+      <Technologies />
     </div>
   );
 }
---
+++ src/helpers/techs.js
@@ -0,0 +1,30 @@
+export const techs = [
+    {
+        name: 'JavaScript',
+        iconName: 'javascript'
+    },
+    {
+        name: 'HTML5',
+        iconName: 'html5'
+    },
+    {
+        name: 'React',
+        iconName: 'react'
+    },
+    {
+        name: 'CSS3',
+        iconName: 'css3'
+    },
+    {
+        name: 'MongoDB',
+        iconName: 'mongodb'
+    },
+    {
+        name: 'Boostrap',
+        iconName: 'bootstrap'
+    },
+    {
+        name: 'GitHub',
+        iconName: 'github_badge'
+    }
+]
